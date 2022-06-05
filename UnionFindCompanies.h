b9289661e85bf32c45815143dd3df9c7182915df
MahamidOmar <omarm@campus.technion.ac.il>
2022-06-05 19:06:40 +0300

    Changes by aymanbarham

--- UnionFindCompanies.h
+++ UnionFindCompanies.h
@@ -7,21 +7,45 @@
 #include "Company.h"
 #include <memory>
 #include <iostream>
+
 using std::shared_ptr;
 
 class UnionFindCompanies
 {
 private:
+
     struct UnionTreeNode {
         shared_ptr<UnionTreeNode> father;
         shared_ptr<Company> company;
-        double value;
+        double valueExtra;
         int mainCompanyID;
     };
-public:
-    void union(Card& card1, Card& card2)
-    {
 
+    UnionTreeNode* members;
+    int k;
+
+public:
+    explicit UnionFindCompanies(int k): members(new UnionTreeNode[k + 1]), k(k){
+        for (int i = 1; i <= k; ++i) {
+            members[i].company = new Company(i, i);
+            members[i].father = nullptr;
+            members[i].valueExtra = 0;
+            members[i].mainCompanyID = i;
+        }
+    }
+
+    shared_ptr<Company> findCompany(int id) {
+        return nullptr;
+    }
+
+    void unionCompanies(int acquirerID, int targetID)
+    {
+        shared_ptr<Company> acquirer = findCompany(acquirerID);
+        shared_ptr<Company> target = findCompany(targetID);
+
+        if (acquirer == target) {
+
+        }
     }
 
 
