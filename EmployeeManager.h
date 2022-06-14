320c8df96e82d556be1bac936c3b1f474be5ebba
MahamidOmar <omarm@campus.technion.ac.il>
2022-06-14 17:06:36 +0300

    v 2.2

--- EmployeeManager.h
+++ EmployeeManager.h
@@ -282,16 +282,17 @@ public:
             }
 
 //            *((long double*) averageBumpGrade) = sumOfGrades / numOfEmployees;
-            printf("AverageBumpGradeBetweenSalaryByGroup: %.1f\n", (double) (sumOfGrades / numOfEmployees));
+            ////requested
+//            printf("AverageBumpGradeBetweenSalaryByGroup: %.1f\n", (double) (sumOfGrades / numOfEmployees));
 
-//            printf("AverageBumpGradeBetweenSalaryByGroup: %.1f\n", floor(10 * ((double) (sumOfGrades / numOfEmployees)) + 0.5f) / 10);
-//            double averageBumpGrade = (double)sumOfGrades/(double)numOfEmployees;
-//            long temp = (long)(averageBumpGrade * 10);
-//            double average = ((double)temp)/10;
-//            if(std::abs(average + 0.1 - averageBumpGrade) <= 0.0000000001){
-//                average += 0.1;
-//            }
-//            printf("AverageBumpGradeBetweenSalaryByGroup: %.1f\n", average);
+            printf("AverageBumpGradeBetweenSalaryByGroup: %.1f\n", floor(10 * ((double) (sumOfGrades / numOfEmployees)) + 0.5f) / 10);
+            double averageBumpGrade = (double)sumOfGrades/(double)numOfEmployees;
+            long temp = (long)(averageBumpGrade * 10);
+            double average = ((double)temp)/10;
+            if(std::abs(average + 0.1 - averageBumpGrade) <= 0.0000000001){
+                average += 0.1;
+            }
+            printf("AverageBumpGradeBetweenSalaryByGroup: %.1f\n", average);
         } catch (...){//only bad alloc
             return ALLOCATION_ERROR;
         }
